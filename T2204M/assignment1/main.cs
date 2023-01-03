9b0fa223f11d3faced0a9e076ed61d0430c01581
danh2707 <105687037+danh2707@users.noreply.github.com>
2023-01-03 23:01:24 +0700

    assignment1

---
+++ T2204M/assignment1/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2204M.assignment1
+{
+    internal class main
+    {
+        public static void Main(String[] args)
+        {
+            phanso f = new phanso();
+        }
+    }
+}
---
+++ T2204M/assignment1/phanso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2204M.assignment1
+{
+     class phanso
+    {
+        private int TuSo, MauSo;
+
+    }
+}
