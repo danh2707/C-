f1487913ec1ded3abfa349f6b64417c2aa4f76ca
danh2707 <105687037+danh2707@users.noreply.github.com>
2023-01-03 11:39:02 +0700

    first code

---
+++ T2204M/Program.cs
@@ -0,0 +1,30 @@
+﻿public class  Program
+{
+    public static void Main(string[] args)
+    {
+        int x = 10;
+        double y = 3.14;
+        Console.WriteLine("Hello world:" + x);
+        string s = "T2204M";
+
+        int[] ns = new int[3];
+        ns[0] = 15;
+        ns[1] = 20;
+        ns[2] = 25;
+
+        foreach(int n in ns)
+        {
+            //n
+        }
+        for (int i =0; i< ns.Length; i++)
+        {
+            //ns[i]
+        }
+
+        string input = Console.ReadLine();//scan
+
+        int num = Convert.ToInt32(input);
+        Console.WriteLine("num=" + num);
+
+    }
+}
